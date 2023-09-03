8e4caf0f7793aa93afade3933b1bf39fbe15b51f
yasminnur <yasminnurazmina@gmail.com>
2023-09-04 00:19:59 +0700

    not yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ module.exports = {
     extend: {
       boxShadow: {
         custom: '4px 4px 24px 0px rgba(0, 0, 0, 0.10)',
+        custom1: '2px 4px 16px 0px rgba(0, 0, 0, 0.15)'
       },
       borderRadius: {
         custom: '67px 40px 40px 40px',
+        custom1: '24px 24px 0px 0px',
+        custom2: '0px 0px 24px 24px',
+      },
+      borderColor: {
+        custom: 'rgba(0, 0, 0, 0.30)',
       }
     },
   },
